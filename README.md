fec0288d226f43b87bb59bf42b5564649c110566
yong seock <myongdol132@gmail.com>
2023-08-28 00:28:48 +0900

    자동 바인딩 데코레이터 추가 및 사용하기

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-##메모
+## 메모
 [DOM 요소 선택 및 OOP 렌더링](https://blog.naver.com/ares132/223195100978)  
 [DOM 요소 와 상호작용](https://blog.naver.com/ares132/223195120994)  
+[자동바인딩 데코레이터](https://blog.naver.com/ares132/223195144352)  
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+
+// 자동 bind 데코레이터
+function 자동바인드(_: any, _2: string, descriptor: PropertyDescriptor) {
+  
+  const 기존메서드 = descriptor.value;
+  const 조정된Dec:PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = 기존메서드.bind(this);
+      return boundFn;
+    }
+  }
+  return 조정된Dec;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -27,13 +43,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @자동바인드
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this))
+    this.element.addEventListener('submit', this.submitHandler)
   }
 
   private attach() {
