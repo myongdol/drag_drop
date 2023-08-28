02183b1d52726deaabde277bea04d7b62c879dce
yong seock <myongdol132@gmail.com>
2023-08-28 23:49:09 +0900

    사용자가 입력한 정보 가져오기

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [DOM 요소 선택 및 OOP 렌더링](https://blog.naver.com/ares132/223195100978)  
 [DOM 요소 와 상호작용](https://blog.naver.com/ares132/223195120994)  
 [자동바인딩 데코레이터](https://blog.naver.com/ares132/223195144352)  
+[사용자가 입력한 정보 가져오기](https://blog.naver.com/ares132/223196109596)  
--- src/app.ts
+++ src/app.ts
@@ -43,10 +43,33 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number]|void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if(enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+      alert('잘못된 입력 입니다. 다시 시도 해주세요.')
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @자동바인드
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if(Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people); 
+      this.clearInput();
+    }
   }
 
   private configure() {
