6628162e53d44477903757a4e86b04126ffce297
yong seock <myongdol132@gmail.com>
2023-08-29 01:28:36 +0900

    재사용 가능한 검증기능 생성하고 적용

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// 검증기능 (Validation) 
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+
+  if(validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+
+  return isValid;
+}
+
 
 // 자동 bind 데코레이터
 function 자동바인드(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -48,7 +80,30 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    if(enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if (
+      // validate({value: enteredTitle, required: true, minLength: 5}) &&
+      // validate({value: enteredDescription, required: true, minLength: 5}) &&
+      // validate({value: enteredTitle, required: true, minLength: 5})
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
       alert('잘못된 입력 입니다. 다시 시도 해주세요.')
     } else {
       return [enteredTitle, enteredDescription, +enteredPeople]
