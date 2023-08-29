1ae5aa6298c7fd114add7ef06614a43e99ae97b9
yong seock <myongdol132@gmail.com>
2023-08-30 03:05:53 +0900

    싱글톤 패턴으로 어플리케이션 상태관리 하기

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 [재사용 가능한 검증 기능 생성하기](https://blog.naver.com/ares132/223196175747)  
 
 [타입에 따라 그에 맞는 목록 렌더링하기](https://blog.naver.com/ares132/223197177521)  
+[싱글톤 패턴으로 어플리케이션 상태관리](https://blog.naver.com/ares132/223197214468)  
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,42 @@
+// 프로젝트 상태 관리 Management
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {
+
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn)
+  }
+
+  addProject(title: string, description: string, numOfPeple: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      people: numOfPeple
+    };
+    this.projects.push(newProject);
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
+
 // 검증기능 (Validation) 
 interface Validatable {
   value: string | number;
@@ -50,12 +89,14 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
+  assignedProjects: any[];
 
   constructor(private type: 'active'|'finished') {
     this.templateElement = document.getElementById(
       'project-list'
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.assignedProjects = [];
 
     const importedNode = document.importNode(
       this.templateElement.content,
@@ -63,8 +104,23 @@ class ProjectList {
     );
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+
+      projectState.addListener((projects: any[]) => {
+        this.assignedProjects = projects;
+        this.renderProjects();
+      });
+
     this.attach();
     this.renderContent();
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement('li');
+      listItem.textContent = prjItem.title;
+      listEl?.appendChild(listItem);
+    }
   }
 
   private renderContent() {
@@ -156,7 +212,7 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if(Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
-      console.log(title, desc, people); 
+      projectState.addProject(title, desc, people);
       this.clearInput();
     }
   }
